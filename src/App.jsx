9cb3fccb77e277912937df2f3ed6438f6308ec11
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-07 22:19:58 +0200

    development

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,20 @@
-// import { Route, BrowserRouter, Routes } from "react-router-dom"
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import Home from "./pages/Home"
-// import Contact from "./pages/Contact"
-// import Error from "./pages/Error"
-
-// const App = () => {
-
-
-
-//   return (
-//     <BrowserRouter>
-//       <main>
-//         <ToastContainer stacked/>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/contact" element={<Contact />} />
-//           <Route path="*" element={<Error />} />
-//         </Routes>
-//       </main>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App
-
-import DefaultLayout from "./layouts/DefaultLayout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <Router>
+      <ToastContainer stacked/>
       <Routes>
-        <Route path="/" element={<DefaultLayout />} />
+        {/* Default Layout */}
+        <Route path="/*" element={<DefaultLayout />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,19 +1,24 @@
-import { Route } from "react-router-dom"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import Home from "../pages/Home"
-import Contact from "../pages/Contact"
+import { Routes, Route, Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+
 const DefaultLayout = () => {
     return (
         <div>
-            <Header />
-            <main>
-                <Route path="/" component={Home} />
-                <Route path="/contact" component={Contact} />
-            </main>
-            <Footer />
+        <Header />
+        <main>
+            <Routes>
+                <Route index element={<Home />} /> {/* "/" route, same as the following */} 
+                {/* <Route path="/" element={<Home />} /> "/" route */}
+                <Route path="contact" element={<Contact />} />
+            </Routes>
+            <Outlet /> {/* Ensures nested routes can still render if needed */}
+        </main>
+        <Footer />
         </div>
-    )
-}
+    );
+};
 
-export default DefaultLayout
+export default DefaultLayout;
