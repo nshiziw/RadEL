e566487fd17cd9695784cee27b949b52681e0fd4
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-11 12:40:45 +0200

    development

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,8 +27,8 @@ const About = () => {
     }
 
     return (
-        <section className="py-10 px-[10%] grid grid-cols-2">
-            <div className="px-[10%] relative group/item group/play">
+        <section className="py-10 px-[10%] grid grid-cols-2 gap-10">
+            <div className="relative group/item group/play">
                 <video id="aboutVideo" className="h-full object-cover outline -outline-offset-[25px] outline-snow" muted={true} src={AboutVideo}></video>
                 {isPlaying ? (
                     <div className="absolute opacity-0 duration-500 group-hover/play:opacity-100 transition top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-5 backdrop-blur-sm group-hover/item:backdrop-blur-xl border-2 rounded-full">
@@ -40,35 +40,35 @@ const About = () => {
                     </div>
                 )}
             </div>
-            <div>
-                <div className="title">
-                    <div>
-                        <p>about us</p>
-                        <h1>the art and soul behind our jewelry</h1>
+            <div className="flex flex-col gap-8">
+                <div className="title grid grid-cols-3 gap-3">
+                    <div className="col-span-2 flex flex-col gap-5">
+                        <p className="uppercase text-3xl font-medium text-gold">about us</p>
+                        <h1 className="text-[45px] capitalize font-italiana text-navy-green">the art and soul behind our jewelry</h1>
                     </div>
-                    <img src="https://media.istockphoto.com/id/1944923205/photo/close-up-of-the-wedding-rings-of-the-bride-and-groom-in-a-box.jpg?s=612x612&w=0&k=20&c=wAGhhCQD0KMyZy6uDUVdTSEYjUXmH49znFShw1V_VG4=" alt="" />
+                    <img className="outline -outline-offset-[10px] outline-snow h-full" src="https://media.istockphoto.com/id/1944923205/photo/close-up-of-the-wedding-rings-of-the-bride-and-groom-in-a-box.jpg?s=612x612&w=0&k=20&c=wAGhhCQD0KMyZy6uDUVdTSEYjUXmH49znFShw1V_VG4=" alt="" />
                 </div>
-                <p>Thank you very much. For he is bound by the pleasures of repudiation, and the expedient elders know nothing of his will for him?</p>
-                <div>
-                    <div>
-                        <h1>20+</h1>
-                        <p>Categories</p>
+                <p className="text-slate-600 text-lg">Thank you very much. For he is bound by the pleasures of repudiation, and the expedient elders know nothing of his will!</p>
+                <div className="grid grid-cols-3 divide-x divide-slate-300 bg-navy-green p-8">
+                    <div className="flex flex-col items-center">
+                        <h1 className="font-italiana text-gold text-4xl">20+</h1>
+                        <p className="text-snow text-lg font-extralight">Categories</p>
                     </div>
-                    <div>
-                        <h1>6000+</h1>
-                        <p>Products</p>
+                    <div className="flex flex-col items-center">
+                        <h1 className="font-italiana text-gold text-4xl">6000+</h1>
+                        <p className="text-snow text-lg font-extralight">Products</p>
                     </div>
-                    <div>
-                        <h1>99%</h1>
-                        <p>Satisfied customer</p>
+                    <div className="flex flex-col items-center">
+                        <h1 className="font-italiana text-gold text-4xl">99%</h1>
+                        <p className="text-snow text-lg font-extralight">Satisfied customer</p>
                     </div>
                 </div>
                 <div>
-                    <p>Wilson Nshizirungu</p>
-                    <div>
-                        <p className="font-Corinthia">Wilson Nshizirungu</p>
-                        <div></div>
-                        <p>Founder of RaEl</p>
+                    <p className="font-Corinthia font-bold text-3xl text-navy-green">Wilson Nshizirungu</p>
+                    <div className="flex items-center gap-1">
+                        <p className="text-slate-600">Wilson Nshizirungu</p>
+                        <div className="w-[6px] h-[6px] bg-gold rounded-full"></div>
+                        <p className="text-slate-600">Founder of RaEl</p>
                     </div>
                 </div>
             </div>
