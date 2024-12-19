04b83d023837b4c2a056d46e79056b375737cc34
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-19 16:04:38 +0200

    development

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,8 @@
 const Title = ({ title, source }) => {
     return (
-        <section className="py-10 px-[10%]">
-            <h1 className="text-rose-500">{title}</h1>
-            <p>{source} / {title}</p>
+        <section className="py-10 px-[10%] flex flex-col gap-3 items-center bg-slate-300">
+            <h1 className="font-italiana text-5xl capitalize text-navy-green">{title}</h1>
+            <p className="text-slate-600 capitalize font-medium">{source} / {title}</p>
         </section>
     );
 };
