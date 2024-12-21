0d262bdc8586465ac4203dfc019309f7530f3a62
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-21 23:20:24 +0200

    development

--- src/components/CollectionsTrendy.jsx
+++ src/components/CollectionsTrendy.jsx
@@ -23,13 +23,13 @@ const CollectionsTrendy = () => {
                         </div>
                     </div>
                     ) : (
-                    <div key={collection.id}>
-                        <div className="relative group">
+                    <div className="border group border-navy-green" key={collection.id}>
+                        <div className="relative overflow-hidden">
                             <img src={collection.image} alt="" />
-                            <div>
-                                <p>30% off</p>
+                            <div className="absolute top-5 left-5">
+                                <p className="bg-gold text-snow px-2 text-sm py-1">30% off</p>
                             </div>
-                            <div className="icons absolute -right-11 group-hover:right-4 top-4 transition-all duration-700 flex flex-col gap-3">
+                            <div className="icons absolute -right-11 group-hover:right-4 top-4 transition-all duration-500 flex flex-col gap-3">
                                 <div className='border bg-snow shadow-lg group/item p-2 rounded-full hover:border-navy-green hover:bg-navy-green transition duration-300 cursor-pointer'>
                                     <CiHeart className='text-2xl text-navy-green group-hover/item:text-snow transition duration-300' />
                                 </div>
@@ -41,18 +41,18 @@ const CollectionsTrendy = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="content w-1/2 p-3 flex flex-col gap-3 justify-center">
+                        <div className="content p-3 flex flex-col gap-3 justify-center">
                             <div className="flex items-center justify-between">
-                                <p className="text-slate-600 text-lg font-semibold">Bracelet</p>
-                                <div className="flex items-center gap-2">
-                                    <FaStar className="text-gold text-xl"/>
-                                    <p className="text-xl font-semibold text-navy-green">4.9</p>
+                                <p className="text-slate-600 text-base capitalize font-semibold">{collection.category}</p>
+                                <div className="flex items-center gap-1">
+                                    <FaStar className="text-gold text-lg"/>
+                                    <p className="text-base font-semibold text-navy-green">{collection.rate}</p>
                                 </div>
                             </div>
-                            <Link className="text-xl capitalize font-semibold text-navy-green" to="/shop">gold bracelet</Link>
+                            <Link className="text-lg capitalize font-semibold text-navy-green" to="/shop">{collection.name}</Link>
                             <div className="flex gap-2">
-                                <p className="text-xl text-navy-green font-medium">$120</p>
-                                <p className="text-xl text-slate-500 line-through font-medium">$200</p>
+                                <p className="text-lg text-navy-green font-medium">{collection.newPrice}</p>
+                                <p className="text-lg text-slate-500 line-through font-medium">{collection.newPrice}</p>
                             </div>
                         </div>
                     </div>
