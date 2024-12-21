724de0589f0b90b3c83356cc59d895fa7ed34a95
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-21 23:50:18 +0200

    development

--- src/components/Cyber.jsx
+++ src/components/Cyber.jsx
@@ -4,7 +4,7 @@ const Cyber = () => {
     return (
         <section className="px-[10%] py-10">
             <div className="relative">
-                <img className="w-full h-64 object-cover" src="https://media.istockphoto.com/id/1174210371/photo/portrait-beautiful-woman-with-jewelry.jpg?s=612x612&w=0&k=20&c=WNV__DU0e6_itmJED9DwyHn7iyVP_IjffitMedOLqMw=" alt="" />
+                <img className="border w-full h-64 object-cover" src="https://www.unoaerre.it/media/wysiwyg/UNOAERREnatale_3_.png" alt="" />
                 <div className="absolute bottom-1/2 left-0 transform ml-10 translate-y-1/2 flex flex-col gap-2 w-1/3">
                     <p className="text-gold uppercase text-xl">cyber monday sale</p>
                     <p className="text-navy-green capitalize font-italiana text-4xl">cyber monday&apos;s best jewelry deals await you</p>
