b4dfb43e2d1561aa03faa71ced59fbff20703ba9
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-22 18:25:17 +0200

    development

--- src/pages/auth/Error.jsx
+++ src/pages/auth/Error.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Error = () => {
   return (
     <section className="flex items-center justify-center flex-col gap-6">
-      <h1 className="text-[200px] font-bold bg-slate-600 bg] errorText">404</h1>
+      <h1 className="text-[200px] font-bold errorText">404</h1>
       <p className="text-slate-600 text-3xl font-semibold -mt-12">Oops! Page not Found</p>
       <p className="text-slate-600 font-medium">The page you are looking for can not be found. Take a break before trying again</p>
       <Link to="/" className="bg-navy-green text-white py-2 px-4 hover:bg-navy-green-light transition duration-300">Back to Home</Link>
