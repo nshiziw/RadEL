fd1d7b319a5846244f094b7d29c9fc2bab3a4e42
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-25 18:17:04 +0200

    development

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,7 +1,14 @@
-
+import { BsClipboardCheck } from "react-icons/bs";
+import { BsClipboard } from "react-icons/bs";
+import { LuBox } from "react-icons/lu";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { FaCheckSquare } from "react-icons/fa";
 const Progress = () => {
     return (
-        <section className="py-10 border border-navy-green my-4"></section>
+        <section className="py-10 border border-navy-green my-4">
+
+        </section>
     )
 }
 
