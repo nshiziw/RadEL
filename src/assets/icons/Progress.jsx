ba8d9d8461204c22502de41ac4652784fc7816be
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-25 18:38:23 +0200

    development

---
+++ src/assets/icons/Progress.jsx
@@ -0,0 +1,15 @@
+// src/assets/data/icons.js
+import { BsClipboardCheck, BsClipboard } from "react-icons/bs";
+import { LuBox } from "react-icons/lu";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { CiDeliveryTruck } from "react-icons/ci";
+
+const icons = {
+    BsClipboardCheck: BsClipboardCheck,
+    BsClipboard: BsClipboard,
+    LuBox: LuBox,
+    LiaShippingFastSolid: LiaShippingFastSolid,
+    CiDeliveryTruck: CiDeliveryTruck,
+};
+
+export default icons;
--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,22 +1,38 @@
-import { BsClipboardCheck } from "react-icons/bs";
-import { BsClipboard } from "react-icons/bs";
-import { LuBox } from "react-icons/lu";
-import { LiaShippingFastSolid } from "react-icons/lia";
-import { CiDeliveryTruck } from "react-icons/ci";
-import { FaCheckSquare } from "react-icons/fa";
-import Progresses from "../assets/data/Progress.json"
+import icons from "../assets/icons/Progress.jsx"
+import Progresses from "../assets/data/Progress.json";
+
 const Progress = () => {
     return (
         <section className="py-10 px-8 border border-navy-green my-4">
-            <div>
-                {Progresses.map((progress) => (
-                    <div key={progress.id}>
-                        <p>{progress.date}</p>
-                    </div>
-                ))}
+            <div className="grid grid-cols-5">
+                {Progresses.map((progress) => {
+                    const IconComponent = icons[progress.icon];
+                    return (
+                        <div key={progress.id} className="flex flex-col justify-center items-center">
+                            <div className="flex flex-col items-center">
+                                {progress.isComplete ? (
+                                    <div className="relative">
+                                        <div className="absolute bottom-1 right-[2px] text-3xl text-navy-green">
+                                            {IconComponent && <IconComponent />}
+                                        </div>
+                                        <div className="w-5 h-5 bg-gold rounded-full"></div>
+                                    </div>
+                                ): (
+                                    <div className="relative">
+                                        <div className="absolute bottom-1 right-[2px] text-3xl text-navy-green">
+                                            {IconComponent && <IconComponent />}
+                                        </div>
+                                        <div className="w-5 h-5 bg-transparent rounded-full"></div>
+                                    </div>
+                                )}
+                                <p>{progress.title}</p>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </section>
     )
 }
 
-export default Progress
+export default Progress;
