520a2f623aa6088c185b4ea3b9a9d1aecb9ded9c
nshizirungu wilson <wilsonshizirungu@gmail.com>
2024-12-26 18:26:12 +0200

    development

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,12 @@
 import Features from "../components/Features"
 import Newsletter from "../components/Newsletter"
+import {Title} from "../components/Title.jsx"
 
 const Contact = () => {
   return (
     <main>
       <section>
-        <h1>This is contact page</h1>
+        <Title source="home" title="contact us" />
       </section>
       <Features />
       <Newsletter />
